6baf6c47b6dc765bc84d9cbc0b569feef3a49789
jansarba <jansarbasarba@gmail.com>
2024-10-24 20:21:11 +0200

    lab3 done

--- oolab/check_for_jackass.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Decode the base64 text and search for 'jackass'
-decoded_text=$(openssl enc -base64 -A -d < home-screen-text.txt)
-echo "$decoded_text" | grep -q 'jackass'
-if [ $? -eq 0 ]; then
-    exit 1  # Found 'jackass', this is a bad commit
-else
-    exit 0  # Did not find 'jackass', this is a good commit
-fi
