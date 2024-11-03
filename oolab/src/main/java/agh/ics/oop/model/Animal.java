a05ac55c6ab51c6089c5c87663193ffb0f8efb1c
jansarba <jansarbasarba@gmail.com>
2024-11-03 17:11:52 +0100

    lab4 done

--- oolab/src/main/java/agh/ics/oop/model/Animal.java
+++ oolab/src/main/java/agh/ics/oop/model/Animal.java
@@ -1,29 +1,53 @@
 package agh.ics.oop.model;
 
+import agh.ics.oop.MoveValidator;
+import agh.ics.oop.model.Vector2d;
+import agh.ics.oop.model.RectangularMap;
+
 public class Animal {
     public MapDirection orientation;
     public Vector2d position;
     public final int index;
+
     public Animal(MapDirection orientation, Vector2d position, int index) {
         this.orientation = orientation;
         this.position = position;
         this.index = index;
     }
+    public Animal(Vector2d position, int index) { //overloading tez
+        this(MapDirection.NORTH, position, index);
+    }
     public Animal(){ //default constructor to trzeba tak zoverloadowac chyba??
         this(MapDirection.NORTH, new Vector2d(2,2),127); //jakis bezpieczny index choc nie wiem czy nie lepiej wywalac bledu przy braku indeksu?
     }
+
+    public Vector2d getPosition(){
+        return position;
+    }
+
+    public void setPosition(Vector2d newPosition){
+        this.position = newPosition;
+    }
+
     @Override
     public String toString() {
-        return "Zwierzę " + index + ": " + position;
+        return switch (orientation) { // Schematyczna orientacja zwierzęcia
+            case NORTH -> "N";
+            case EAST -> "E";
+            case SOUTH -> "S";
+            case WEST -> "W";
+        };
     }
+
     public boolean isAt(Vector2d position){
         return this.position.equals(position);
     }
-    public boolean isInside(Vector2d position){ // metoda bardziej prewencyjna wiec jakbym korzystal z this.position to by bylo juz za pozno
-        return (position.follows(MapDirection.LOWER_LEFT_BOUNDARY) & position.precedes(MapDirection.UPPER_RIGHT_BOUNDARY));
+
+    public boolean isInside(Vector2d position){
+        return (position.follows(RectangularMap.getLOWER_LEFT_BOUNDARY()) & position.precedes(RectangularMap.getUPPER_RIGHT_BOUNDARY()));
     }
 
-    public void move(MoveDirection direction){
+    public void move(MoveDirection direction, MoveValidator validator){
         Vector2d forward_vec = switch (orientation) { //patrzymy, gdzie jest "przod"
             case NORTH -> MapDirection.NORTH_VEC;
             case EAST -> MapDirection.EAST_VEC;
@@ -33,13 +57,15 @@ public class Animal {
 
         switch (direction){ //jesli forward/backward to idziemy
             case FORWARD:
-                if (isInside(position.add(forward_vec))){ // brzydkie chyba?
-                    this.position=position.add(forward_vec);
+                Vector2d newPosition_1 = position.add(forward_vec);
+                if (validator.canMoveTo(newPosition_1)) { // Using MoveValidator to check if the move is valid
+                    this.position = newPosition_1;
                 }
                 break;
             case BACKWARD:
-                if (isInside(position.add(forward_vec.opposite()))){
-                    this.position=position.add(forward_vec.opposite());
+                Vector2d newPosition_2 = position.add(forward_vec.opposite());
+                if (validator.canMoveTo(newPosition_2)) {
+                    this.position = newPosition_2;
                 }
                 break;
             default: break;
@@ -49,7 +75,7 @@ public class Animal {
             case RIGHT -> orientation.next();
             case LEFT -> orientation.previous();
             case FORWARD -> orientation;
-            case BACKWARD -> orientation.next().next(); //brzydkie?
+            case BACKWARD -> orientation.next().next();
         };
     }
 }
--- oolab/src/test/java/agh/ics/oop/SimulationTest.java
+++ oolab/src/test/java/agh/ics/oop/SimulationTest.java
@@ -1,9 +1,6 @@
 package agh.ics.oop;
 
-import agh.ics.oop.model.Animal;
-import agh.ics.oop.model.MapDirection;
-import agh.ics.oop.model.MoveDirection;
-import agh.ics.oop.model.Vector2d;
+import agh.ics.oop.model.*;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -16,8 +13,9 @@ class SimulationTest{
     void animalHasCorrectOrientation(){
         List<MoveDirection> moves=List.of(MoveDirection.FORWARD,MoveDirection.RIGHT,MoveDirection.FORWARD,MoveDirection.LEFT);
         List<Vector2d> positions=List.of(new Vector2d(2,2));
-        Simulation simulation=new Simulation(positions,moves);
-        simulation.run();
+        RectangularMap map = new RectangularMap(10, 10);
+        Simulation simulation = new Simulation(positions, moves);
+        simulation.run(map);
 
         Animal animal=simulation.getAnimals().get(0);
         assertEquals(MapDirection.NORTH,animal.orientation); // Po ostatnim ruchu powinno zostać NORTH
@@ -27,8 +25,9 @@ class SimulationTest{
     void animalMovesToCorrectPosition(){
         List<MoveDirection> moves=List.of(MoveDirection.FORWARD,MoveDirection.FORWARD);
         List<Vector2d> positions=List.of(new Vector2d(2,2));
-        Simulation simulation=new Simulation(positions,moves);
-        simulation.run();
+        RectangularMap map = new RectangularMap(10, 10);
+        Simulation simulation = new Simulation(positions, moves);
+        simulation.run(map);
 
         Animal animal=simulation.getAnimals().get(0);
         assertEquals(new Vector2d(2,4),animal.position); // Po dwóch ruchach FORWARD z pozycji (2,2) zwierzę powinno być na (2,4)
@@ -37,14 +36,15 @@ class SimulationTest{
     @Test
     void animalStaysInsideMap(){
         List<MoveDirection> moves=List.of(MoveDirection.FORWARD,MoveDirection.FORWARD,MoveDirection.FORWARD,MoveDirection.FORWARD);
-        List<Vector2d> positions=List.of(new Vector2d(2,MapDirection.UPPER_RIGHT_BOUNDARY.getXY()[1] - 1));
-        Simulation simulation=new Simulation(positions,moves);
-        simulation.run();
+        List<Vector2d> positions=List.of(new Vector2d(2,MapDirection.UPPER_RIGHT_BOUNDARY.getY() - 1));
+        RectangularMap map = new RectangularMap(10, 10);
+        Simulation simulation = new Simulation(positions, moves);
+        simulation.run(map);
 
         Animal animal=simulation.getAnimals().get(0);
         // Sprawdzenie, czy zwierzę pozostaje w granicach mapy
         assertTrue(animal.isInside(animal.position));
-        assertEquals(new Vector2d(2,MapDirection.UPPER_RIGHT_BOUNDARY.getXY()[1]),animal.position); // Zwierzę nie powinno wyjść poza górną granicę
+        assertEquals(new Vector2d(2,MapDirection.UPPER_RIGHT_BOUNDARY.getY()),animal.position); // Zwierzę nie powinno wyjść poza górną granicę
     }
 
     @Test
